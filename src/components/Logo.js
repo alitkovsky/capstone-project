a4ed5f94b87c9af702086eccd7f36dda33ad668c
alitkovsky <alitkovsky@me.com>
2023-09-06 18:12:56 +0200

    Add images folder. And components acording to the semantich layout.

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+   return (
+      <>
+         <img src="../images/Logo.svg" alt="Little Lemon Restaurant" />
+      </>
+   );
+};
+
+export default Logo;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+conts Main = () => {
+   return (
+      <main>
+         <h1>React Router Demo</h1>
+         <p>By: Your Name</p>
+      </main>
+   );
+};
+
+export default Main;
