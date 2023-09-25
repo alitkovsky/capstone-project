44e36b649aaffd7bf33a1f13091259a7e3046f7e
alitkovsky <alitkovsky@me.com>
2023-09-25 13:36:41 +0200

    Added Testimonials component.

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,7 +1,64 @@
+import React from 'react';
+
+import './Testimonials.css';
+import TestimonialCard from './TestimonialCard';
+
+import client1 from './assets/clientPhoto1.png';
+import client2 from './assets/clientPhoto2.png';
+import client3 from './assets/clientPhoto3.png';
+
+const testimonials = [
+   {
+      name: 'Jane',
+      lastName: 'Airinous',
+      rating: '★★★★',
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.`,
+      clientImage: client1,
+      slogan: 'It was a very good experience'
+   },
+   {
+      name: 'Jane',
+      lastName: 'Doesun',
+      rating: '★★★★★',
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.`,
+      clientImage: client2,
+      slogan: 'It was a very good experience'
+   },
+   {
+      name: 'John',
+      lastName: 'Smithson',
+      rating: '★★★',
+      review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu.`,
+      clientImage: client3,
+      slogan: 'It was a very good experience'
+   }
+];
+
 const Testimonials = () => {
    return (
       <>
-      
+         <section className="testimonials grid">
+            <div className="testimonialsContainer grid container">
+               <div className="testimonialsTitle grid">
+                  <h1>What our clients say about us</h1>
+               </div>
+               <div className="testimonialsCards grid">
+
+                  {testimonials.map((testimonial, index) => (
+                     <TestimonialCard
+                        key={index}
+                        // className={index === 1 ? 'middle-card' : ''}
+                        name={testimonial.name}
+                        lastName={testimonial.lastName}
+                        rating={testimonial.rating}
+                        review={testimonial.review}
+                        image={testimonial.clientImage}
+                        slogan={testimonial.slogan}
+                     />
+                  ))}
+               </div>
+            </div>
+         </section>
       </>
    )
 };
